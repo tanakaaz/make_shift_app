9ad4dd8a88a08dba634b6923ec189a4ddcfa0c7e
tanakaaz <20233084-matsumurachisei@hcs.ac.jp>
2025-05-09 05:27:34 +0000

    テスト　成功

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # make_shift_app
 シフト作成WEBアプリ
+
+Thymeleaf メモ
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+model.addAttribute("message","mictest");
+th:text="${message}"
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,9 +1,14 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 @Controller
-public class HelloController {
+public class MainController {
 
     @GetMapping("/")
-    public String sayHello() {
+    public String sayHello(Model model) {
         System.out.println("Aa");
+        model.addAttribute("message","いぇえ");
         return "index";
     }
 }
